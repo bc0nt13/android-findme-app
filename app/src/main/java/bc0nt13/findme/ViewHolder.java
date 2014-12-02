2abb09400efafd2ef512bae3905f2609424e1a9d
Bruno Conti <53541286+bc0nt13@users.noreply.github.com>
2014-12-02 14:14:19 -0500

    Updated to use appcompat

--- app/src/main/java/bc0nt13/findme/ViewHolder.java
+++ app/src/main/java/bc0nt13/findme/ViewHolder.java
@@ -1,8 +1,16 @@
 package bc0nt13.findme;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
 
-class ViewHolder {
+class ViewHolder extends RecyclerView.ViewHolder {
     TextView name;
+
+    public ViewHolder(View itemView) {
+        super(itemView);
+
+        name = (TextView) itemView.findViewById(R.id.room_name);
+    }
 }
 
