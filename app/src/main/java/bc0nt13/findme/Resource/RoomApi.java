c931d0dc3af56b407e645252776ac549d0cc50eb
Bruno Conti <53541286+bc0nt13@users.noreply.github.com>
2014-12-02 15:09:46 -0500

    Used retrofit.Callback instead of AsyncTask

--- app/src/main/java/bc0nt13/findme/Resource/RoomApi.java
+++ app/src/main/java/bc0nt13/findme/Resource/RoomApi.java
@@ -2,10 +2,12 @@ package bc0nt13.findme.Resource;
 
 import java.util.List;
 
+import retrofit.Callback;
+
 import bc0nt13.findme.Room;
 import retrofit.http.*;
 
 public interface RoomApi {
     @GET("/rooms")
-    List<Room> list();
+    void list(Callback<List<Room>> callback);
 }
